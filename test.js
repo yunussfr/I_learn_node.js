c1032c59965715b29e45394c29f2c658d2e73a25
Yunus Emre Dertli <yunus23456drt@gmail.com>
2026-03-08 21:15:39 +0300

    Iadded node.js variable declare

---
+++ test.js
@@ -0,0 +1,55 @@
+// bu bir yorum satırıdır
+console.log("merhabalar"); //konsola yazı yazdırmak için kullanılır
+/*yorum satırı1
+yorum satırı2*/
+/* 
+
+*burası önemli */
+console.log("acaba ne kadar iyi node.js bilebilirim");//!bu satır kaldırılacaktır
+// TODO: konsola 3 yazdırılacak
+// primitive data types (ilkel veri tipleri)-Reference / object
+//Number:sayi
+//String:metin
+//Boolean:true/false
+//undefined:değişken tanımlanır ama değer ataması yapılmazsa undefined olur
+//null:değişken tanımlanır ve değer ataması yapılmazsa null olur
+//object:birden fazla değeri tek bir değişkende tutmak için kullanılır
+//array:diziler
+//function:bir işlemi gerçekleştirmek için kullanılan kod bloklarıdır
+// bigInt:çok büyük sayıları tutmak için kullanılır
+//symbol:benzersiz ve değiştirilemez bir değeri temsil eder 
+
+//variable - Identifier
+// Declare-Assigment
+
+//Number
+var tetete3=1
+
+//String
+var tetete2="merhaba"
+
+//Boolean
+var tetete1=true
+
+//BigInt
+var tetete=1234567890123456789012345678901234567890n
+
+//BigInt ile int arasında işlem yapamazsın dönüştürme yapman lazım
+
+//Symbol
+
+let test=Symbol("test2")
+
+//null
+
+let test6=null;
+
+//undefined
+let test7;
+// new ve for bu kodlama dilinde önceden tanımlanmıştır değişkenlerini buna göre tanımlamaman gereklidir bu isimler ile
+console.log("bu değer:",test6);
+console.log("bu büyük sayı:",tetete);
+console.log(test7);
+
+//typeof
+console.log(typeof tetete);
